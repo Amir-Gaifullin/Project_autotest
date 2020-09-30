b06d36e11c7f2fbae09a624cad34ce86176ddf0d
Amir Gayfullin <amir.gaifullin@yandex.ru>
2020-09-30 19:01:01 +0300

    test4

--- project/test.py
+++ project/test.py
@@ -1,4 +1,3 @@
-from fractions import Fraction
 import unittest
 from my_sum import sum
 class TestSum(unittest.TestCase):
@@ -9,5 +8,12 @@ class TestSum(unittest.TestCase):
         data = [1, 2, 3]
         result = sum(data)
         self.assertEqual(result, 6)
+    def test_list_fraction(self):
+        """
+        Test that it can sum a list of fractions
+        """
+        data = [Fraction(1, 4), Fraction(1, 4), Fraction(2, 5)]
+        result = sum(data)
+        self.assertEqual(result, 1)
 if __name__ == '__main__':
     unittest.main()
